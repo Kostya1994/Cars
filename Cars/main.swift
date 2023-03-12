d6f6be878e8dea5fc0f9301a245511f8ef798fe8
SoloviovKN <qwwwwwq7@gmail.com>
2023-03-12 21:15:20 +0100

    new protocol car draft

--- Cars/main.swift
+++ Cars/main.swift
@@ -42,225 +42,181 @@ enum colors {
     case black
 }
 
-class Car {
-    let model : String
-    let year : UInt
-    let doors : UInt
-    let wheels : UInt
-    let trunkVolume : UInt
-    var freeVolumeInTrunk: UInt{
-        get{
-            return trunkVolume - baggageVolumSum
-        }
-    }
-    var baggage  : [UInt]
-    {
-        willSet{
-            print("The baggage will update. \(baggage) and \(freeVolumeInTrunk)")
-           }
-        didSet{
-            print("The baggage was updated. \(baggage) and \(freeVolumeInTrunk)")
-           }
-    }
+protocol Car {
+    var model : String {set get}
+    var year : UInt {set get}
+    var doors : UInt {set get}
+    var wheels : UInt {set get}
+    var trunkVolume : UInt {set get}
+    var baggage  : [UInt] {set get}
+    var carSuspension : suspension {set get}
+    var speedMax : UInt {set get}
+    var navigation : navigationSystem {set get}
+    var transmission : transmissionOption {set get}
+    var autoBlockDoors: UInt {set get}
+    var speed : UInt {set get}
+    var doorsState : securityState {set get}
+    var lights : lightsState {set get}
+    var autoEngine : engineState {set get}
     
-    var baggageVolumSum : UInt{
-        get{
-            var count: UInt = 0
-            for i in 0..<baggage.count{
-                count += baggage[i]
-            }
-            return count
-        }
-    }
+    var color : colors {set get}
 
-    let carSuspension : suspension
-    let speedMax : UInt
-    let navigation : navigationSystem
-    let transmission : transmissionOption
-    private let autoBlockDoors: UInt = 20
-    var speed : UInt{
-    willSet (newSpeed){
-        if newSpeed >= autoBlockDoors{
-            self.doorsState = .blocked
-            print("Doors \(self.doorsState) becouse speed are hight than \(autoBlockDoors)")
-        }
-        return
+//    //func startAutoEngine(_ carLights: lightsState)
+//
+//    func stoppedAutoEngine(_ carLights: lightsState)
+//
+//    func slowDownForStop()
+//
+//    func carIsMovingWithSpeed(speed: UInt)
+//
+//    func isCarReadyToStop()
+//
+//    func doorsState(state: securityState)
+//
+//    func putInTrunk(newBag: UInt)
+//
+//    func getFromTrunk(myBag: UInt)
+//    func printAboutBad(answer : Bool, bag: UInt)
+//
+    
+}
+extension Car{
+    var freeVolumeInTrunk: UInt{
+    get{
+        return trunkVolume - baggageVolumSum
     }
 }
-    private var doorsState : securityState
-    var lights : lightsState
-    var autoEngine : engineState {
-        didSet{
-            print("Auto engine = \(autoEngine)")
+    var baggageVolumSum : UInt{
+    get{
+        var count: UInt = 0
+        for i in 0..<baggage.count{
+            count += baggage[i]
         }
+        return count
     }
-    var color : colors
-    var isReadyToMove : Bool {
+}
+    var isReadyToMove : Bool{
         get{
             return autoEngine == .started ? true : false
         }
     }
-    private var isReadyToStop : Bool {
-        get{
-            return speed == 0 ? true : false
-        }
+    var isReadyToStop : Bool{
+    get{
+        return speed == 0 ? true : false
     }
-    init(model: String, year: UInt, doors: UInt, wheels: UInt, trunkVolume: UInt, carSuspension: suspension, speedMax: UInt, navigation: navigationSystem, transmission: transmissionOption, color: colors) {
-        self.model = model
-        self.year = year
-        self.doors = doors
-        self.wheels = wheels
-        self.baggage = []
-        self.trunkVolume = trunkVolume
-        self.carSuspension = carSuspension
-        self.speedMax = speedMax
-        self.navigation = navigation
-        self.transmission = transmission
-        self.speed = 0
-        self.doorsState = .unblocked
-        self.lights = .turnedOff
-        self.autoEngine = .stopped
-        self.color = color
+}
+    mutating func startAutoEngine(_ carLights: lightsState) {
+    switch carLights{
+    case .turnedOn:
+        autoEngine = .started
+        self.lights = .turnedOn
+        print("Is car ready to move? Answer: \(isReadyToMove == true ? "Yes" : "No")")
+    case .turnedOff:
+        autoEngine = .started
+        self.lights = .turnedOn
+        print("Is car ready to move? Answer: \(isReadyToMove == true ? "Yes" : "No")")
     }
     
-    func startAutoEngine(_ carLights: lightsState) {
-        switch carLights{
-        case .turnedOn:
-            autoEngine = .started
-            self.lights = .turnedOn
-            print("Is car ready to move? Answer: \(isReadyToMove == true ? "Yes" : "No")")
-        case .turnedOff:
-            autoEngine = .started
-            self.lights = .turnedOn
-            print("Is car ready to move? Answer: \(isReadyToMove == true ? "Yes" : "No")")
-        }
-        
+}
+
+    mutating func stoppedAutoEngine(_ carLights: lightsState) {
+    guard isReadyToStop == true else {
+      return  print("Car is not ready to stop. You need slow down the car and stopped.")
     }
-    
-    func stoppedAutoEngine(_ carLights: lightsState) {
-        guard isReadyToStop == true else {
-          return  print("Car is not ready to stop. You need slow down the car and stopped.")
-        }
-        switch carLights{
-        case .turnedOn:
-            isCarReadyToStop()
-            autoEngine = .stopped
-            self.lights = .turnedOn
-        case .turnedOff:
-            isCarReadyToStop()
-            autoEngine = .stopped
-            self.lights = .turnedOff
-        }
-        
-    }
-    
-    func slowDownForStop() {
-        if self.speed > 20 {
-            while self.speed > 20 {
-                self.speed -= 15
-                print("My speed is \(self.speed)")
-                sleep(1)
-            }
-        }
-        print("I'm ready to stopped. My speed is \(self.speed)")
-        self.speed -= self.speed
+    switch carLights{
+    case .turnedOn:
         isCarReadyToStop()
-        print("I've just stopped. My speed is \(self.speed).")
+        autoEngine = .stopped
+        self.lights = .turnedOn
+    case .turnedOff:
+        isCarReadyToStop()
+        autoEngine = .stopped
+        self.lights = .turnedOff
     }
     
-    func carIsMovingWithSpeed(speed: UInt) {
-        guard isReadyToMove == true else {
-          return  print("Car is not ready to move. Start car.")
+}
+
+    mutating func slowDownForStop() {
+    if self.speed > 20 {
+        while self.speed > 20 {
+            self.speed -= 15
+            print("My speed is \(self.speed)")
+            sleep(1)
         }
-        switch Int(speed){
-        case 0:
-            self.speed = speed
-            print("Not enought speed for moving")
-        case 1...Int(self.speedMax):
-            self.speed = speed
-            print("Car is moving with speed = \(self.speed)")
-        default:
-            self.speed = self.speedMax - 1
-            print("Car speed is \(self.speed) becouse car's max = \(self.speedMax)")
-            
-        }
+    }
+    print("I'm ready to stopped. My speed is \(self.speed)")
+    self.speed -= self.speed
+    isCarReadyToStop()
+    print("I've just stopped. My speed is \(self.speed).")
+}
+
+    mutating func carIsMovingWithSpeed(speed: UInt) {
+    guard isReadyToMove == true else {
+      return  print("Car is not ready to move. Start car.")
+    }
+    switch Int(speed){
+    case 0:
+        self.speed = speed
+        print("Not enought speed for moving")
+    case 1...Int(self.speedMax):
+        self.speed = speed
+        print("Car is moving with speed = \(self.speed)")
+    default:
+        self.speed = self.speedMax - 1
+        print("Car speed is \(self.speed) becouse car's max = \(self.speedMax)")
         
     }
     
-    func isCarReadyToStop(){
-        print("Is car ready to stop? Answer: \(isReadyToStop)")
+}
+
+    mutating func isCarReadyToStop(){
+    print("Is car ready to stop? Answer: \(isReadyToStop)")
+}
+
+    mutating func doorsState(state: securityState){
+    guard speed < self.autoBlockDoors else {
+        return print("Too hight speed. Slow down to the \(autoBlockDoors - 1) speed")
     }
-    
-    func doorsState(state: securityState){
-        guard speed < self.autoBlockDoors else {
-            return print("Too hight speed. Slow down to the \(autoBlockDoors - 1) speed")
-        }
-        self.doorsState = state
-        print("The doors = \(self.doorsState)")
+    self.doorsState = state
+    print("The doors = \(self.doorsState)")
+}
+
+    mutating func putInTrunk(newBag: UInt){
+    guard newBag <= freeVolumeInTrunk else {
+        return print("There is no free place for your bag. Free place = \(freeVolumeInTrunk)")
     }
-    
-    func putInTrunk(newBag: UInt){
-        guard newBag <= freeVolumeInTrunk else {
-            return print("There is no free place for your bag. Free place = \(freeVolumeInTrunk)")
-        }
-        self.baggage.append(newBag)
-        print("Your put something in the trank. Free place = \(freeVolumeInTrunk)")
+    self.baggage.append(newBag)
+    print("Your put something in the trank. Free place = \(freeVolumeInTrunk)")
+}
+
+    mutating func getFromTrunk(myBag: UInt){
+    guard self.baggageVolumSum > 0  else {
+        return print("There is nothing in your trunk. Free place = \(freeVolumeInTrunk)")
     }
-    
-    func getFromTrunk(myBag: UInt){
-        guard self.baggageVolumSum > 0  else {
-            return print("There is nothing in your trunk. Free place = \(freeVolumeInTrunk)")
+    let newArray = self.baggage
+    var thing : Bool = false
+    for i in (0..<newArray.count){
+        if newArray[i] == myBag{
+            self.baggage.remove(at: i)
+            thing = true
+            break
         }
-        let newArray = self.baggage
-        var thing : Bool = false
-        for i in (0..<newArray.count){
-            if newArray[i] == myBag{
-                self.baggage.remove(at: i)
-                thing = true
-                break
-            }
-        }
-        printAboutBad(answer: thing, bag: myBag)
     }
-    func printAboutBad(answer : Bool, bag: UInt){
-        if answer == true {
-            print("Here you are = \(bag). Free place = \(freeVolumeInTrunk)")
-        }else{
-            print("There is no your bag = \(bag). Free place = \(freeVolumeInTrunk)")
-        }
+    printAboutBad(answer: thing, bag: myBag)
+}
+    mutating func printAboutBad(answer : Bool, bag: UInt){
+    if answer == true {
+        print("Here you are = \(bag). Free place = \(freeVolumeInTrunk)")
+    }else{
+        print("There is no your bag = \(bag). Free place = \(freeVolumeInTrunk)")
     }
 }
     
+}
+
     
-var myFirstCar : Car = Car(model: "Ford", year: 2001, doors: 5, wheels: 4, trunkVolume: 200, carSuspension: .helicalCoilSprings, speedMax: 180, navigation: .gpsNavigator, transmission: .automatic, color: .yellow)
 
 
-//myFirstCar.carIsMovingWithSpeed(speed: 100)
-//myFirstCar.startAutoEngine(.turnedOn)
-//myFirstCar.carIsMovingWithSpeed(speed: 220)
-//print(myFirstCar.lights)
-//
-//print("----------------")
-//
-//myFirstCar.stoppedAutoEngine(.turnedOn)
-//myFirstCar.doorsState(state: .unblocked)
-//print("----------------")
-//myFirstCar.carIsMovingWithSpeed(speed: 19)
-//myFirstCar.doorsState(state: .unblocked)
-//myFirstCar.carIsMovingWithSpeed(speed: 190)
-
-//myFirstCar.slowDownForStop()
-
-//myFirstCar.stoppedAutoEngine(.turnedOn)
-//myFirstCar.putInTrunk(newBag: 20)
-//myFirstCar.putInTrunk(newBag: 20)
-//myFirstCar.putInTrunk(newBag: 19)
-//myFirstCar.putInTrunk(newBag: 161)
-//myFirstCar.putInTrunk(newBag: 1)
-//
-//print("----------------")
-//myFirstCar.getFromTrunk(myBag: 19)
-//myFirstCar.getFromTrunk(myBag: 20)
 
 enum turboOpt : UInt {
     case zero = 0
@@ -269,16 +225,84 @@ enum turboOpt : UInt {
     case three = 25
 }
 
-final class SportCar : Car {
+class SportCar : Car {
+    
+    var doorsState: securityState
+        
+    var model: String
+    
+    var year: UInt
+    
+    var doors: UInt
+    
+    var wheels: UInt
+    
+    var trunkVolume: UInt
+    
+    var baggage: [UInt]
+        {
+            willSet{
+                print("The baggage will update. \(baggage) and \(freeVolumeInTrunk)")
+               }
+            didSet{
+                print("The baggage was updated. \(baggage) and \(freeVolumeInTrunk)")
+               }
+        }
+    var carSuspension: suspension
+    
+    var speedMax: UInt
+    
+    var navigation: navigationSystem
+    
+    var transmission: transmissionOption
+    
+    var autoBlockDoors: UInt
+    
+    var speed: UInt
+        {
+        willSet (newSpeed){
+            if newSpeed >= autoBlockDoors{
+                self.doorsState = .blocked
+                print("Doors \(self.doorsState) becouse speed are hight than \(autoBlockDoors)")
+            }
+            return
+        }
+    }
+    var lights: lightsState
+    
+    var autoEngine: engineState
+        {
+            didSet{
+                print("Auto engine = \(autoEngine)")
+            }
+        }
+    var color: colors
+     
     let myTurbo : Bool
     var turbo : turboOpt
-    init(model: String, year: UInt, doors: UInt, speedMax: UInt, color: colors, myTurbo: Bool) {
-        self.turbo = .zero
+    
+    init(doorsState: securityState, model: String, year: UInt, doors: UInt, wheels: UInt, trunkVolume: UInt, baggage: [UInt], carSuspension: suspension, speedMax: UInt, navigation: navigationSystem, transmission: transmissionOption, autoBlockDoors: UInt, speed: UInt, lights: lightsState, autoEngine: engineState, color: colors, myTurbo: Bool, turbo: turboOpt) {
+        self.doorsState = doorsState
+        self.model = model
+        self.year = year
+        self.doors = doors
+        self.wheels = wheels
+        self.trunkVolume = trunkVolume
+        self.baggage = baggage
+        self.carSuspension = carSuspension
+        self.speedMax = speedMax
+        self.navigation = navigation
+        self.transmission = transmission
+        self.autoBlockDoors = autoBlockDoors
+        self.speed = speed
+        self.lights = lights
+        self.autoEngine = autoEngine
+        self.color = color
         self.myTurbo = myTurbo
-        super.init(model: model, year: year, doors: doors, wheels: 4, trunkVolume: 0, carSuspension: .helicalCoilSprings, speedMax: speedMax, navigation: .secondDriver, transmission: .manual, color: color)
+        self.turbo = turbo
     }
     
-    override func carIsMovingWithSpeed(speed: UInt) {
+     func carIsMovingWithSpeed(speed: UInt) {
         guard isReadyToMove == true else {
             return  print("Car is not ready to move. Start car.")
         }
@@ -314,7 +338,7 @@ final class SportCar : Car {
     }
 }
 
-var firstSportCar : SportCar = SportCar(model: "ford", year: 200, doors: 2, speedMax: 280, color: .white, myTurbo: true)
+var firstSportCar : SportCar = SportCar(doorsState: .blocked, model: "Ford", year: 2004, doors: 4, wheels: 4, trunkVolume: 100, baggage: 20, carSuspension: <#T##suspension#>, speedMax: <#T##UInt#>, navigation: <#T##navigationSystem#>, transmission: <#T##transmissionOption#>, autoBlockDoors: <#T##UInt#>, speed: <#T##UInt#>, lights: <#T##lightsState#>, autoEngine: <#T##engineState#>, color: <#T##colors#>, myTurbo: <#T##Bool#>, turbo: <#T##turboOpt#>)
 
 firstSportCar.startAutoEngine(.turnedOn)
 firstSportCar.carIsMovingWithSpeed(speed: 200)
